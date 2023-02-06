f76ef65c6f78b17fd2103bf705a60437eb1eb863
DasherSlade <nahuel4002@gmail.com>
2023-02-05 21:17:21 -0300

    mock de tareas y su interface

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import {Task} from "./task"
+
+export const TASK: Task[] = [
+    {
+        id:1,
+        text: "Terminar el modulo 3",
+        day: 'Febrero 7 a las 00:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Terminar el modulo 4",
+        day: 'Febrero 20 a las 00:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Terminar el modulo 5",
+        day: 'Marzo 1 a las 00:00',
+        reminder: false
+    },
+    {
+        id:4,
+        text: "Terminar el modulo 6",
+        day: 'Marzo 15 a las 00:00',
+        reminder: true
+    }
+]
